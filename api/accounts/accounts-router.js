de7bf694ca387ed9b60a87f24c0dc494f94a2fdf
mgrobstein <97206147+mgrobstein@users.noreply.github.com>
2022-06-03 02:33:24 -0400

    Finished everything

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,44 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try{
+    throw new Error('argh!!!')
+  } catch (err) {
+    next({ status: 422, message: 'this is horrible'})// DO YOUR MAGIC
+}})
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try{
+    
+  } catch (err) {
+    next(err)// DO YOUR MAGIC
+}})
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try{
+    
+  } catch (err) {
+    next(err)// DO YOUR MAGIC
+}})
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+  try{
+    
+  } catch (err) {
+    next(err)// DO YOUR MAGIC
+}});
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try{
+    
+  } catch (err) {
+    next(err)// DO YOUR MAGIC
+}})
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message:err.message,
+  })
 })
 
 module.exports = router;
